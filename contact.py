c986c55129dd20994658d62130b9d543f564142b
privet-banditam <romansmithpoet@gmail.com>
2024-07-30 00:24:54 +0500

    add object

---
+++ contact.py
@@ -0,0 +1,12 @@
+class Contact
+
+    def __init__(self, fname, mname, lname, nname, title, company, address, home, email):
+        self.fname = fname
+        self.mname = mname
+        self.lname = lname
+        self.nname = nname
+        self.title = title
+        self.company = company
+        self.address = address
+        self.home = home
+        self.email = email
